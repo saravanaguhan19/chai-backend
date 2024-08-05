7a503bf79dc670fcb20cbc72114b5692d6d75b12
saravanaguhan19 <saravanaguhan19@gmail.com>
2024-08-05 12:17:03 +0530

    fix all register controller bugs

--- src/db/index.js
+++ src/db/index.js
@@ -1,21 +1,23 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
 
-const connectDB = async ()=> {
-    try {
-        const connectionInstance = await  mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-        
-        // const connectionInstance = await  mongoose.connect(`${}/${DB_NAME}`);
+    // const connectionInstance = await  mongoose.connect(`${}/${DB_NAME}`);
 
-        console.log(connectionInstance);
-        
+    // console.log(connectionInstance);
 
-        console.log(`\n MongoDb connected !! DB HOST: ${connectionInstance.connection.host}`)
-    } catch (error) {
-        console.log("MONGODB connection error ",error);
-        process.exit(1);
-    }
-}
+    console.log(
+      `\n MongoDb connected !! DB HOST: ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("MONGODB connection error ", error);
+    process.exit(1);
+  }
+};
 
 export default connectDB;
